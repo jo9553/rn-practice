e28ce4cefbfd8c4a7e182b259591231acdf6a985
jo9553 <61581051+jo9553@users.noreply.github.com>
2022-11-21 10:53:44 +1300

    Add rn-paper_menu
    Understand the menu components.

---
+++ rn-paper_menu/app.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import CustMenu from './custMenu';
+
+
+const MyComponent = () => {
+
+  return (
+      <View>
+        <CustMenu />
+        <Text>this is Text</Text>
+      </View>
+  )
+};
+
+
+export default MyComponent;
